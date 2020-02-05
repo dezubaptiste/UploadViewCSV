cd5baccf3c39c2f6e568fa0a0d73a46c16ac7d92
dezubaptiste <dezubaptiste@gmail.com>
2020-02-05 17:50:06 +0530

    Add files via upload

---
+++ UploadCSV/Read_CSV_MVC/Models/EmployeeModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Read_CSV_MVC.Models
+{
+    public class EmployeeModel
+    {
+        ///<summary>
+        /// Gets or sets CustomerId.
+        ///</summary>
+        public int EmployeeId { get; set; }
+
+        ///<summary>
+        /// Gets or sets Name.
+        ///</summary>
+        public string Name { get; set; }
+
+        ///<summary>
+        /// Gets or sets Country.
+        ///</summary>
+        public string Designation { get; set; }
+    }
+}
